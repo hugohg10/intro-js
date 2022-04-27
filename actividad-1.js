50eff3fed6495ba1e4e0eb9ff8467e388be378e9
Hugo <inv2797@innovaccion.mx>
2022-04-26 21:57:00 -0500

    Ejercicios de intro js

---
+++ actividad-1.js
@@ -0,0 +1,52 @@
+// actividad 1
+// ingresar un número y multiplicarlo por 2.
+
+var numero = prompt ("ingrese un número");
+
+resultado = (numero * 2);
+
+console.log(resultado);
+
+
+
+//Actividad 2
+//ingresar dos numero y restarlos.
+
+var numero1 = prompt ("ingrese el primer número");
+var numero2 = prompt ("ingrese el segundo número");
+
+resultado = (numero1-numero2);
+
+console.log(resultado);
+
+//actividad 3
+//carcular area de circulo con radio 5.3.
+
+var radio = 5.3;
+
+const pi = 3.14
+
+var formula = pi * radio **2;
+
+console.log(formula);
+
+88.2026 //resultado
+
+
+
+//Activida 4
+
+//Calcular area de un rombo con los siguimiente datos.
+
+var D = 8;
+var d = 6;
+formula =  D * d / 2;
+area = formula;
+console.log(area);
+
+24 //resultado 
+
+
+
+
+
